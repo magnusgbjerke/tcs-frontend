0c0d2ea8b49502bcf1e482a0072e618b00eb78bd
magnusgbjerke <magnusgbjerke@gmail.com>
2025-01-26 16:01:53 +0100

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <header>Header</header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p>Hello World!</p>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        Footer
-      </footer>
+    <div className="max-w-[1440px] w-full">
+      <p>Hello World! <br/> This is the Home page</p>
     </div>
   );
 }
