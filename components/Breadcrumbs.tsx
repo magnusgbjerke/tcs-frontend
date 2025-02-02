7f224273af4dfe98355fd7799dff6c65c46d76b1
magnusgbjerke <magnusgbjerke@gmail.com>
2025-02-02 11:33:47 +0100

    pull from TCS-UI repo

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -1,21 +1,28 @@
-export interface items {
+"use client";
+
+export interface BreadcrumbItem {
   title: string;
   href: string;
 }
 
-type Props = {
-  items: Array<items> | [];
-};
+interface Props {
+  items: Array<BreadcrumbItem> | [];
+}
+
+function capitalizeFirstLetter(word: string): string {
+  if (!word) return "";
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
 
 export function Breadcrumbs({ items = [] }: Props) {
   return (
     <>
       <div className="flex">
         {items.map((item, index) => (
-          <p key={index}>
-            {item.title}
+          <div key={index}>
+            <a href={item.href}>{capitalizeFirstLetter(item.title)}</a>
             {index === items.length - 1 ? "" : " > "}
-          </p>
+          </div>
         ))}
       </div>
     </>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,20 +2,9 @@
 
 import Link from "next/link";
 import { Searchbar } from "./Searchbar";
+import { mockSearch } from "@/mocks/searchbar";
 
 export const Navbar = () => {
-  const exampleData = [
-    "T-shirt",
-    "Jeans",
-    "Hoodie",
-    "Jacket",
-    "Sneakers",
-    "Boots",
-    "Dress",
-    "Hat",
-    "Scarf",
-    "Socks",
-  ];
   return (
     <header className="bg-primary-400 h-[60px] justify-items-center sticky">
       <div className="max-w-[1440px] w-full">
@@ -35,14 +24,12 @@ export const Navbar = () => {
               <strong>[Button: Kids]</strong>
             </Link>
           </div>
-          <div className="absolute left-1/2 transform -translate-x-1/2">
-            middle
-          </div>
+          <div className="absolute left-1/2 transform -translate-x-1/2"></div>
           <div className="flex">
             <Searchbar
               placeholder={"Search for products..."}
-              data={exampleData}
-              onSearch={(q) => console.log(q)}
+              data={mockSearch}
+              onSearch={() => {}}
               size="xs"
             />
             <p className="text-nowrap">[Icon: Profile]</p>
